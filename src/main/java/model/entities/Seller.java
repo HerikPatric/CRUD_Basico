faf5f6b7173a7be5f1714919ac0cbdd35634f914
HerikPatric <herikpdsilva@gmail.com>
2026-01-29 14:13:40 -0300

    inserção da composição no construtor da classe seller

--- src/main/java/model/entities/Seller.java
+++ src/main/java/model/entities/Seller.java
@@ -14,12 +14,13 @@ public class Seller implements Serializable {
 
     public Seller(){}
 
-    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary) {
+    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
         Id = id;
         this.name = name;
         this.email = email;
         this.birthDate = birthDate;
         this.baseSalary = baseSalary;
+        this.department = department;
     }
 
     public Integer getId() {
