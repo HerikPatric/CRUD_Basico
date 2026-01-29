c69ead8da1049cfb92e878b3da63cb815739a448
HerikPatric <herikpdsilva@gmail.com>
2026-01-28 23:13:25 -0300

    Criacao das classes Department, Seller e DataBase

--- src/main/java/model/entities/Department.java
+++ src/main/java/model/entities/Department.java
@@ -1,2 +1,53 @@
-public class Department {
+package model.entities;
+
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Department implements Serializable {
+    private Integer Id;
+    private String name;
+
+    public Department(Integer id, String name) {
+        Id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(Id, that.Id) && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "Id=" + Id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
 }
--- src/main/java/model/entities/Seller.java
+++ src/main/java/model/entities/Seller.java
@@ -1,4 +1,84 @@
 package model.entities;
 
-public class Seller {
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Seller implements Serializable {
+    private Integer Id;
+    private String name;
+    private String email;
+    private Date birthDate;
+    private Double baseSalary;
+
+    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary) {
+        Id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.baseSalary = baseSalary;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Seller seller = (Seller) o;
+        return Objects.equals(Id, seller.Id) && Objects.equals(name, seller.name) && Objects.equals(email, seller.email) && Objects.equals(birthDate, seller.birthDate) && Objects.equals(baseSalary, seller.baseSalary);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id, name, email, birthDate, baseSalary);
+    }
+
+    @Override
+    public String toString() {
+        return "Seller{" +
+                "Id=" + Id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDate=" + birthDate +
+                ", baseSalary=" + baseSalary +
+                '}';
+    }
 }
