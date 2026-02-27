c18f199ff173b804ac4f925f2ab63a654225ce19
Herik P. D. Da Silva <herik.silva@MEGASULT.INF>
2026-02-27 16:49:14 -0300

    Insert

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,12 +4,16 @@ import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.util.Date;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
 
+        Date data = new Date();
         SellerDAO sellerDAO = DaoFactory.createSellerDAO();
         System.out.println("=== TESTE 1: Seller findById ===");
         Seller seller = sellerDAO.findById(3);
@@ -21,5 +25,9 @@ public class App {
         System.out.println("\n=== TESTE 3: Seller findAll ===");
         List<Seller> lista = sellerDAO.findAll();
         lista.forEach(System.out::println);
+        System.out.println("\n=== TESTE 4: Seller insert ===");
+        Seller newSeller =  new Seller(null,"Robinho","robinho@binho",data,5000.00,department);
+        //sellerDAO.insert(newSeller);
+       //System.out.println("Inserted! new id: " + newSeller.getId());
     }
 }
